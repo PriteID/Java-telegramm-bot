05004db637bbb6063bd95dc8000397210ebfa563
Prite <volkovmihail013@yandex.ru>
2023-12-04 18:42:36 +0300

    commit 11

--- Java-Telegramm-Bot/src/main/java/TelegramBotApp.java
+++ Java-Telegramm-Bot/src/main/java/TelegramBotApp.java
@@ -1,9 +1,6 @@
 import telegram.Initialization;
-
-
 public class TelegramBotApp {
     public static void main(String[] args) {
-        //Qu
         System.out.println("Инициализация запуска");
         Initialization.start();
     }
