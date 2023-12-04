cd0ca8f6ca64e3d0906ce676fc9bd04fcec79de9
Maks89204 <lmg200489@gmail.com>
2023-12-04 20:27:13 +0500

    Commit1

--- Java-Telegramm-Bot/src/main/java/TelegramBotApp.java
+++ Java-Telegramm-Bot/src/main/java/TelegramBotApp.java
@@ -2,6 +2,7 @@ import telegram.Initialization;
 
 
 public class TelegramBotApp {
+    //а
     public static void main(String[] args) {
         System.out.println("Инициализация запуска");
         Initialization.start();
