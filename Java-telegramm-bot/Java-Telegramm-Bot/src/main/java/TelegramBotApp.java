7e52e1eab42487e8cca48996653f764b31f4a3c5
PriteID <114207475+PriteID@users.noreply.github.com>
2023-12-18 21:07:44 +0300

    Delete Java-telegramm-bot directory

--- Java-telegramm-bot/Java-Telegramm-Bot/src/main/java/TelegramBotApp.java
+++
@@ -1,8 +0,0 @@
-import telegram.Initialization;
-
-public class TelegramBotApp {
-    public static void main(String[] args) {
-        System.out.println("Инициализация запуска");
-        Initialization.start();
-    }
-}
